c538efb7b3aa871a7ffe827b45364b3befc30c0d
batetolast1 <batetolast1@gmail.com>
2020-10-17 15:10:24 +0200

    added handling all necessary actions in BookController

--- src/main/java/io/github/batetolast1/bookapi/controllers/BookController.java
+++ src/main/java/io/github/batetolast1/bookapi/controllers/BookController.java
@@ -1,15 +1,56 @@
 package io.github.batetolast1.bookapi.controllers;
 
-import io.github.batetolast1.bookapi.domain.entities.Book;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.batetolast1.bookapi.domain.entities.Response;
+import io.github.batetolast1.bookapi.domain.entities.enums.Status;
+import io.github.batetolast1.bookapi.dtos.BookDTO;
+import io.github.batetolast1.bookapi.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
-    @RequestMapping("/helloBook")
-    public Book helloBook() {
-        return new Book(1L, "9788324631766", "Thinking in Java", "Bruce Eckel", "Helion", "programming");
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("")
+    public Response<List<BookDTO>> getAllBooks() {
+        return new Response<>(Status.OK, bookService.getAllBooks());
+    }
+
+    @PostMapping("")
+    public Response<BookDTO> saveNewBook(@RequestBody BookDTO bookDTO) {
+        return new Response<>(Status.OK, bookService.saveNewBook(bookDTO));
+    }
+
+    @PutMapping("")
+    public Response<BookDTO> editBook(@RequestBody BookDTO bookDTO) {
+        BookDTO editedBookDTO = bookService.editBook(bookDTO);
+        if (editedBookDTO == null) {
+            return new Response<>(Status.NOT_FOUND, null);
+        }
+        return new Response<>(Status.OK, editedBookDTO);
+    }
+
+    @GetMapping("/{id:\\d+}")
+    public Response<BookDTO> getBookById(@PathVariable("id") Long id) {
+        BookDTO bookDTO = bookService.getBookById(id);
+        if (bookDTO == null) {
+            return new Response<>(Status.NOT_FOUND, null);
+        }
+        return new Response<>(Status.OK, bookDTO);
+    }
+
+    @DeleteMapping("/{id:\\d+}")
+    public Response<Boolean> deleteBookById(@PathVariable("id") Long id) {
+        boolean isDeleted = bookService.deleteBookById(id);
+        return isDeleted ? new Response<>(Status.OK, Boolean.TRUE) : new Response<>(Status.NOT_FOUND, Boolean.FALSE);
     }
 }
