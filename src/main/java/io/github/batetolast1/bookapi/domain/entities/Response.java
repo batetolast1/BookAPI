0d43ad45b66476016890f59ff79daa885fa26d72
batetolast1 <batetolast1@gmail.com>
2020-10-17 15:00:12 +0200

    created Response entity

---
+++ src/main/java/io/github/batetolast1/bookapi/domain/entities/Response.java
@@ -0,0 +1,30 @@
+package io.github.batetolast1.bookapi.domain.entities;
+
+import io.github.batetolast1.bookapi.domain.entities.enums.Status;
+
+public class Response<T> {
+
+    private Status status;
+    private T payload;
+
+    public Response(Status status, T payload) {
+        this.status = status;
+        this.payload = payload;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public T getPayload() {
+        return payload;
+    }
+
+    public void setPayload(T payload) {
+        this.payload = payload;
+    }
+}
---
+++ src/main/java/io/github/batetolast1/bookapi/domain/entities/enums/Status.java
@@ -0,0 +1,5 @@
+package io.github.batetolast1.bookapi.domain.entities.enums;
+
+public enum Status {
+    OK, NOT_FOUND
+}
